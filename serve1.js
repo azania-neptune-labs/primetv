8117f4644a7d02bb42be4fafd8ec90302494772b
Andile Mchunu <47420082+SkywalkingZulu@users.noreply.github.com>
2019-11-18 10:57:09 +0200

    Create serve1.js

---
+++ serve1.js
@@ -0,0 +1,35 @@
+var process = require('process');  
+var http = require('http'),
+    fs = require('fs'),
+    util = require('util');
+
+
+http.createServer(function (req, res) {
+  var path = 'video1.mp4';
+  var stat = fs.statSync(path);
+  var total = stat.size;
+  if (req.headers['range']) {
+    var range = req.headers.range;
+    var parts = range.replace(/bytes=/, "").split("-");
+    var partialstart = parts[0];
+    var partialend = parts[1];
+
+    var start = parseInt(partialstart, 10);
+    var end = partialend ? parseInt(partialend, 10) : total-1;
+    var chunksize = (end-start)+1;
+    console.log('RANGE: ' + start + ' - ' + end + ' = ' + chunksize);
+
+    var file = fs.createReadStream(path, {start: start, end: end});
+    res.writeHead(206, { 'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
+    file.pipe(res);
+  } else {
+    console.log('ALL: ' + total);
+    res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
+    fs.createReadStream(path).pipe(res);
+  }
+}).listen(1337, );
+console.log('Server running at http://localhost:1337/');
+setTimeout(function() {
+	process.abort();
+    console.log('Blah blah blah blah extra-blah');
+}, 300000);
